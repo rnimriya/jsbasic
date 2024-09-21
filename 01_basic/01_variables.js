1bf45b0b144b7d1ad302b05e387f0e2a44b52e94
Rakesh Kumar <r.nimriya@gmail.com>
2024-09-21 05:24:55 +0000

    variable

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,12 @@
+const accountID = 145251;
+let accountEmail = "testing@gmail.com"
+var accountPassword = "abc@123#"
+accountCity = "Delhi"
+let accountState ;
+
+// accountID = 14523; we can't change const vairable
+
+console.log(accountID)
+
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState ])
+
