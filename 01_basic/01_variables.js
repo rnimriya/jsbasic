433f243b97cce76a7bd953382d7f130300750013
Rakesh Kumar <r.nimriya@gmail.com>
2024-09-24 06:07:09 +0000

    coversion

--- 01_basic/01_variables.js
+++ 01_basic/01_variables.js
@@ -1,8 +1,8 @@
 const accountID = 145251;
 let accountEmail = "testing@gmail.com"
-var accountPassword = "abc@123#"
-accountCity = "Delhi"
-let accountState ;
+var accountPassword = "abc@123#" // prefer not use var variable 
+accountCity = "Delhi" // we can store value without var, let and const keyword in javascript
+let accountState ;  // if we can't assign a value of variable so 
 
 // accountID = 14523; we can't change const vairable
 
